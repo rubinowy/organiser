319e68f6f1cd6399ebf7d0e3541ae5e02f7ef439
Justyna Mianowska <emily.hepburn90@gmail.com>
2014-02-18 20:42:11 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Organiser::Application.config.secret_key_base = '978e7231ebdee9db22516788cea1437143a72add4c0cc34a107c53d6379e70c92b311132831f667b121bf711794ca78d0667b32683043e8cdcea3f70e57cb960'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Organiser::Application.config.session_store :cookie_store, key: '_organiser_session'
